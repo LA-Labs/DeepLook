dcc7110144a4b5bfb8f730a39cdeb6d684f4e687
amir-lahav <lalabsproduct@gmail.com>
2022-05-14 10:32:14 +0300

    DeepLookAPI: fix docs

--- Sources/DeepLook/API/ImageProcessor.swift
+++ Sources/DeepLook/API/ImageProcessor.swift
@@ -16,56 +16,67 @@ import UIKit
 public let ImageProcessor = LKImageProcessor.default
 
 public class LKImageProcessor {
-    
-    // shared instance
-    public static let `default` = LKImageProcessor()
-    
-    // initiate only once.
-    private init() { }
-    
-    public func alignFaces(in sourceImages: UIImage...,
-                           processConfiguration: ProcessConfiguration,
-                           completion: @escaping (Result<[Face], VisionProcessError>) -> Void) {
-        let featureDetecting = Actions.cropAndAlignFaces
-        let input = sourceImages.map { (sourceImage) in
-            ProcessAsset(identifier: "sourceImage", image: sourceImage)
-        }.map { (processAsset) -> ProcessInput in
-            ProcessInput(asset: processAsset, configuration: processConfiguration)
-        }.chunked(into: 10)
-        var objects = Stack<[ProcessInput]>()
-        input.forEach { (inputs) in
-            objects.push(inputs)
-        }
-        Vision.detect(objects: objects, process: featureDetecting) { (result) in
-            switch result {
-            case .success(let photos):
-                let faces = photos.map { (asset) -> [Face] in
-                    asset.faces
-                }.flatMap({$0})
-                completion(.success(faces))
-            case .failure(let error):
-                print(error)
-            }
-        }
+
+  // shared instance
+  public static let `default` = LKImageProcessor()
+
+  // initiate only once.
+  private init() { }
+
+  public func alignFaces(in sourceImages: UIImage...,
+                         processConfiguration: ProcessConfiguration = ProcessConfiguration(),
+                         completion: @escaping (Result<[Face], VisionProcessError>) -> Void) {
+    alignFaces(
+      sourceImages: sourceImages,
+      processConfiguration: processConfiguration,
+      completion: completion
+    )
+  }
+
+  public func alignFaces(sourceImages: [UIImage],
+                         processConfiguration: ProcessConfiguration = ProcessConfiguration(),
+                         completion: @escaping (Result<[Face], VisionProcessError>) -> Void) {
+    precondition(!sourceImages.isEmpty, "sourceImages must not be empty")
+    let featureDetecting = Actions.cropAndAlignFaces
+    let input = sourceImages.map { (sourceImage) in
+      ProcessAsset(identifier: "sourceImage", image: sourceImage)
+    }.map { (processAsset) -> ProcessInput in
+      ProcessInput(asset: processAsset, configuration: processConfiguration)
+    }.chunked(into: 10)
+    var objects = Stack<[ProcessInput]>()
+    input.forEach { (inputs) in
+      objects.push(inputs)
     }
-    
-    
-    public func alignFaces(fetchOptions: AssetFetchingOptions,
-                           processConfiguration: ProcessConfiguration,
-                           completion: @escaping (Result<[Face], VisionProcessError>) -> Void) {
-        let featureDetecting = Actions.cropAndAlignFaces
-        let assets = Vision.assetService.stackInputs(with: fetchOptions,
-                                                     processConfiguration: processConfiguration)
-        Vision.detect(objects: assets, process: featureDetecting) { (result) in
-            switch result {
-            case .success(let photos):
-                let faces = photos.map { (asset) -> [Face] in
-                    asset.faces
-                }.flatMap({$0})
-                completion(.success(faces))
-            case .failure(let error):
-                print(error)
-            }
-        }
+    Vision.detect(objects: objects, process: featureDetecting) { (result) in
+      switch result {
+      case .success(let photos):
+        let faces = photos.map { (asset) -> [Face] in
+          asset.faces
+        }.flatMap({$0})
+        completion(.success(faces))
+      case .failure(let error):
+        print(error)
+      }
     }
+  }
+
+
+  public func alignFaces(fetchOptions: AssetFetchingOptions,
+                         processConfiguration: ProcessConfiguration,
+                         completion: @escaping (Result<[Face], VisionProcessError>) -> Void) {
+    let featureDetecting = Actions.cropAndAlignFaces
+    let assets = Vision.assetService.stackInputs(with: fetchOptions,
+                                                 processConfiguration: processConfiguration)
+    Vision.detect(objects: assets, process: featureDetecting) { (result) in
+      switch result {
+      case .success(let photos):
+        let faces = photos.map { (asset) -> [Face] in
+          asset.faces
+        }.flatMap({$0})
+        completion(.success(faces))
+      case .failure(let error):
+        print(error)
+      }
+    }
+  }
 }
